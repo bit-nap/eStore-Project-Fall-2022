c0f46193995fb03abb730b65cf5d3f337132cb9a
GiovanniCoppola084 <gac6151@rit.edu>
2022-09-28 20:50:36 -0400

    Create TicketControllerTest.java
    Created the file to test the ticket controller methods made for sprint 1. The method to test getTicket was also added.

---
+++ estore-api/src/test/Controller/TicketControllerTest.java
@@ -0,0 +1,50 @@
+import java.beans.Transient;
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+
+import com.estore.api.estoreapi.controller.TicketController;
+import com.estore.api.estoreapi.model.Ticket;
+import com.estore.api.estoreapi.persistence.TicketDAO;
+
+/**
+ * Test the Ticket Controller class
+ *
+ * @author Group 3c
+ */
+@Tag("Controller-tier")
+public class TicketControllerTest {
+	private TicketController ticketController;
+	private TicketDAO mockTicketDao;
+
+	/**
+	 * Before a test, create a new TicketController object and inject
+	 * a mock Ticket DAO
+	 */
+	@BeforeEach
+	public void setupTicketController() {
+		mockTicketDao = mock(TicketDAO.class);
+		ticketController = new TicketController(mockTicketDao);
+	}
+
+	/**
+	 * Test the getTicket method to get a ticket for a movie based on
+	 * the ticket id
+	 * @throws IOException
+	 */
+	@Test
+	public void testGetTicket() throws IOException {
+		// Setup
+		Ticket ticket = new Ticket(99, "Star Wars");
+		// When the same id is passed in, our mock Ticket DAO will return the Ticket object
+		when(mockTicketDao.getTicket(ticket.getId())).thenReturn(ticket);
+
+		// Invoke
+		ResponseEntity<Ticket> resonse = ticketController.getTicket(ticket.getId());
+
+		// Analyze
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals(ticket, response.getBody());
+	}
+}
+

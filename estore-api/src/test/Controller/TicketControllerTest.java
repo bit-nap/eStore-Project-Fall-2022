6ce20d702c833753e6d2cbad7f4d9c867bbd7157
GiovanniCoppola084 <gac6151@rit.edu>
2022-09-28 21:07:25 -0400

    Update TicketControllerTest.java
    Updated to have the functions for getTickets test.

--- estore-api/src/test/Controller/TicketControllerTest.java
+++ estore-api/src/test/Controller/TicketControllerTest.java
@@ -46,5 +46,64 @@ public class TicketControllerTest {
 		assertEquals(HttpStatus.OK, response.getStatusCode());
 		assertEquals(ticket, response.getBody());
 	}
+
+	@Test
+    public void testGetTicketNotFound() throws Exception { // getTicket may throw IOException
+        // Setup
+        int ticketId = 99;
+        // When the same id is passed in, our mock Ticket DAO will return null, simulating
+        // no hero found
+        when(mockTicketDAO.getTicket(ticketId)).thenReturn(null);
+
+        // Invoke
+        ResponseEntity<Ticket> response = ticketController.getTicket(ticketId);
+
+        // Analyze
+        assertEquals(HttpStatus.NOT_FOUND,response.getStatusCode());
+    }
+
+    @Test
+    public void testGetTicketHandleException() throws Exception { // getTicket may throw IOException
+        // Setup
+        int ticketId = 99;
+        // When getTicket is called on the Mock Ticket DAO, throw an IOException
+        doThrow(new IOException()).when(mockTicketDAO).getTicket(ticketId);
+
+        // Invoke
+        ResponseEntity<Ticket> response = ticketController.getTicket(ticketId);
+
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
+    }
+
+	@Test
+    public void testGetTickets() throws IOException { // getTickets may throw IOException
+        // Setup
+        Ticket[] ticket = new Ticket[2];
+        ticket[0] = new Ticket(99,"Napolean Dynamite");
+        ticket[1] = new Ticket(100,"Warm Bodies");
+        // When getTicket is called return the tickets created above
+        when(mockTicketDAO.getTickets()).thenReturn(ticket);
+
+        // Invoke
+        ResponseEntity<Ticket[]> response = ticketController.getTicket();
+
+        // Analyze
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals(ticket,response.getBody());
+    }
+
+    @Test
+    public void testGetTicketesHandleException() throws IOException { // getTickets may throw IOException
+        // Setup
+        // When getTickets is called on the Mock Ticket DAO, throw an IOException
+        doThrow(new IOException()).when(mockTicketDAO).getTickets();
+
+        // Invoke
+        ResponseEntity<Ticket[]> response = ticketController.getTickets();
+
+        // Analyze
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR,response.getStatusCode());
+    }
 }
 

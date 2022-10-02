0a20116d394f3ee69e6c590f68306d1174b300f3
Oscar Rojas <or8655@rit.edu>
2022-10-02 11:39:58 -0400

    Removing annotation

--- estore-api/src/main/java/com/estore/api/estoreapi/EstoreApiApplication.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/EstoreApiApplication.java
@@ -2,9 +2,7 @@ package com.estore.api.estoreapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = "estoreapi")
 @SpringBootApplication
 public class EstoreApiApplication {
 

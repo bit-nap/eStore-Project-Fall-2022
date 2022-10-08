92db4c902da276b3a6787ad15e46d9529f72265b
Oscar Rojas <or8655@rit.edu>
2022-10-08 13:48:18 -0400

    Updating Movie class with new private fields

--- estore-api/src/main/java/com/estore/api/estoreapi/movies/model/Movie.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/movies/model/Movie.java
@@ -14,27 +14,41 @@ public class Movie {
 	private static final Logger LOG = Logger.getLogger(Movie.class.getName());
 
 	// Package private for tests - Prof
-	static final String STRING_FORMAT = "Movie [id=%d, title=%s]";
+	static final String STRING_FORMAT = "Movie [id=%d, title=%s, runtime=%s, mpaRating=%s, year=%d]";
 
 	/** The id of this movie. */
 	@JsonProperty("id") private int id;
 	/** The title of the movie. */
 	@JsonProperty("title") private String title;
+	/** The runtime of the movie. */
+	@JsonProperty("runtime") private String runtime;
+	/** The Motion Pictures Association (MPA) rating of the movie. */
+	@JsonProperty("mpaRating") private String mpaRating;
+	/** The year the movie was released. */
+	@JsonProperty("year") private int year;
 
 	/**
-	 * Create a Movie object with the given id and movie title.
+	 * Create a Movie object with the given information.
 	 *
-	 * @param id    The id of this movie.
-	 * @param title The title of the movie.
-	 *              <p>
-	 *              {@literal @}JsonProperty is used in serialization and deserialization
-	 *              of the JSON object to the Java object in mapping the fields.  If a field
-	 *              is not provided in the JSON object, the Java field gets the default Java
-	 *              value, i.e. 0 for int
+	 * @param id        The id of this movie.
+	 * @param title     The title of the movie.
+	 * @param runtime   The runtime of the movie.
+	 * @param mpaRating The Motion Pictures Association (MPA) rating of the movie.
+	 * @param year      The year the movie was released.
+	 *                  <p>
+	 *                  {@literal @}JsonProperty is used in serialization and deserialization
+	 *                  of the JSON object to the Java object in mapping the fields.  If a field
+	 *                  is not provided in the JSON object, the Java field gets the default Java
+	 *                  value, i.e. 0 for int
 	 */
-	public Movie (@JsonProperty("id") int id, @JsonProperty("title") String title) {
+	public Movie (@JsonProperty("id") int id, @JsonProperty("title") String title,
+	              @JsonProperty("runtime") String runtime, @JsonProperty("mpaRating") String mpaRating,
+	              @JsonProperty("year") int year) {
 		this.id = id;
 		this.title = title;
+		this.runtime = runtime;
+		this.mpaRating = mpaRating;
+		this.year = year;
 	}
 
 	/**
@@ -45,15 +59,6 @@ public class Movie {
 	}
 
 	/**
-	 * Set the title of the movie.
-	 *
-	 * @param title The title of the movie.
-	 */
-	public void setTitle (String title) {
-		this.title = title;
-	}
-
-	/**
 	 * @return The title of the movie.
 	 */
 	public String getTitle () {
@@ -61,10 +66,31 @@ public class Movie {
 	}
 
 	/**
+	 * @return The runtime of the movie.
+	 */
+	public String getRuntime () {
+		return runtime;
+	}
+
+	/**
+	 * @return The Motion Pictures Association (MPA) rating of the movie.
+	 */
+	public String getMpaRating () {
+		return mpaRating;
+	}
+
+	/**
+	 * @return The year the movie was released.
+	 */
+	public int getYear () {
+		return year;
+	}
+
+	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public String toString () {
-		return String.format(STRING_FORMAT, id, title);
+		return String.format(STRING_FORMAT, id, title, runtime, mpaRating, year);
 	}
 }

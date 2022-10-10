984d4331d119b8ed2b3c768f1c067837570c4217
Oscar Rojas <or8655@rit.edu>
2022-10-09 20:01:08 -0400

    Adding and removing annotations

--- estore-api/src/main/java/com/estore/api/estoreapi/screenings/model/Screening.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/screenings/model/Screening.java
@@ -2,13 +2,9 @@ package com.estore.api.estoreapi.screenings.model;
 
 import com.estore.api.estoreapi.movies.MovieGetter;
 import com.estore.api.estoreapi.movies.model.Movie;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -35,18 +31,14 @@ public class Screening {
 	/** The number of tickets remaining for this screening. */
 	@JsonProperty("ticketsRemaining") private int ticketsRemaining;
 	/** The date of this screening. */
-	@JsonDeserialize(using = LocalDateDeserializer.class)
-	@JsonSerialize(using = LocalDateSerializer.class)
 	@JsonProperty("date") private LocalDate date;
 	/** The time of this screening. */
-	@JsonDeserialize(using = LocalTimeDeserializer.class)
-	@JsonSerialize(using = LocalTimeSerializer.class)
 	@JsonProperty("time") private LocalTime time;
 
 	/** The MovieGetter object used to set this object's movie field. */
-	MovieGetter movieGetter;
+	@JsonIgnore MovieGetter movieGetter;
 	/** The Movie being shown at this screening. */
-	private Movie movie;
+	@JsonIgnore private Movie movie;
 
 	/**
 	 * Create a Screening object with the given id and movie name. The {@linkplain #setMovieGetter} method should be called
@@ -63,6 +55,7 @@ public class Screening {
 	 *                         is not provided in the JSON object, the Java field gets the default Java
 	 *                         value, i.e. 0 for int
 	 */
+	@JsonCreator
 	public Screening (@JsonProperty("id") int id, @JsonProperty("movieId") int movieId,
 	                  @JsonProperty("ticketsRemaining") int ticketsRemaining, @JsonProperty("date") LocalDate date,
 	                  @JsonProperty("time") LocalTime time) {

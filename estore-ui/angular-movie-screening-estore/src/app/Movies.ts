5a22bf5b1a4f18b2d255db6616e06e7df79de0e7
GiovanniCoppola084 <gac6151@rit.edu>
2022-10-10 19:46:49 -0400

    Display movies
    Created the start of the angular portion that will display a list of movies and it's properties. No testing has been done yet.

---
+++ estore-ui/angular-movie-screening-estore/src/app/Movies.ts
@@ -0,0 +1,9 @@
+/// Movies interface to store the values that are in the JSON objects
+export interface Movies {
+  id: number,
+  title: string,
+  poster: string,
+  runtime: string,
+  mpaRating: string,
+  year: number
+}
--- estore-ui/angular-movie-screening-estore/src/app/app.module.ts
+++ estore-ui/angular-movie-screening-estore/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+
+// Http component added for angular so that we can make cURL requests from the server
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule //Http module to allow us to make the cURL calls
   ],
   providers: [],
   bootstrap: [AppComponent]

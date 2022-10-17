e664d5179e24d9b2b3c544fbc8384f72b6a5e7f1
GiovanniCoppola084 <gac6151@rit.edu>
2022-10-17 14:48:07 -0400

    Manually added angular files
    These files were added manually since the github pull request was saying it was already added, although it wasn't

---
+++ estore-ui/angular-movie-screening-estore/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+
+// Http component added for angular so that we can make cURL requests from the server
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MoviesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule, //Http module to allow us to make the cURL calls
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

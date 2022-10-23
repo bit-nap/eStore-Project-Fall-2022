9723f8887d360b6852674364b9627731b5a61526
GiovanniCoppola084 <gac6151@rit.edu>
2022-10-23 18:51:53 -0400

    Button made for the user to sign in
    The username can enter a username to use. The system will check if the account has been made yet. However, something is wrong with how the account is being stored still.

---
+++ estore-ui/angular-movie-screening-estore/src/app/Accounts.ts
@@ -0,0 +1,4 @@
+export interface Accounts {
+  username: string,
+  password: string
+}
--- estore-ui/angular-movie-screening-estore/src/app/login/login.component.ts
+++ estore-ui/angular-movie-screening-estore/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+import { Accounts } from '../Accounts'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: Accounts[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  enterUsername(username: string): void {
+    this.http.get<[Accounts]>('http://127.0.0.1:8080/accounts').subscribe((data: Accounts[]) => {
+      this.username = data;
+    })
+
+    if (this.username.length == 0) {
+      this.http.post<Accounts>('http://127.0.0.1:8080/accounts', username[0]);
+      if (document.getElementById("usernameMessage")) {
+        document.getElementById("usernameMessage")!.innerHTML = "Username created";
+      }
+    }
+    else {
+      if (document.getElementById("usernameMessage")) {
+        document.getElementById("usernameMessage")!.innerHTML = "Username already exists. Please choose another one.";
+      }
+    }
   }
 
 }

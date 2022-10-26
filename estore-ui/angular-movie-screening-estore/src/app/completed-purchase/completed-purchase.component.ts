adc14ca98b517f7b581c3404fc22399d9ea350b6
Oscar Rojas <or8655@rit.edu>
2022-10-26 17:08:45 -0400

    Inserting info from ScreenSelectorService into completed purchase component

--- estore-ui/angular-movie-screening-estore/src/app/completed-purchase/completed-purchase.component.ts
+++ estore-ui/angular-movie-screening-estore/src/app/completed-purchase/completed-purchase.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieSelectorService } from "../movie-selector.service";
+import { ScreeningSelectorService } from "../screening-selector.service";
 
 @Component({
   selector: 'app-completed-purchase',
@@ -7,12 +8,28 @@ import { MovieSelectorService } from "../movie-selector.service";
   styleUrls: ['./completed-purchase.component.css']
 })
 export class CompletedPurchaseComponent implements OnInit {
-  constructor(private movieSelector: MovieSelectorService) { }
+  constructor(private movieSelector: MovieSelectorService, private screeningSelector: ScreeningSelectorService) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
+  /**
+   * Get the last selected movie's title.
+   */
   getMovieTitle(): String {
     return this.movieSelector.getMovieTitle();
+  }
+
+  /**
+   * Get the last selected screening's date.
+   */
+  getScreeningDate(): String {
+    return this.screeningSelector.getScreeningDate();
+  }
+
+  /**
+   * Get the last selected screening's time.
+   */
+  getScreeningTime(): String {
+    return this.screeningSelector.getScreeningTime();
   }
 }

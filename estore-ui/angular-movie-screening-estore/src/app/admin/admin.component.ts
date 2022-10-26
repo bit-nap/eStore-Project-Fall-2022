76458c7e57b03be1c46f5478b8f133d4a2d1b994
GiovanniCoppola084 <gac6151@rit.edu>
2022-10-26 14:06:53 -0400

    Update to admin accounts again
    Some slight changes were made

--- estore-ui/angular-movie-screening-estore/src/app/admin/admin.component.ts
+++ estore-ui/angular-movie-screening-estore/src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Screenings } from '../Screenings';
 
@@ -12,13 +13,13 @@ export class AdminComponent implements OnInit {
   newScreenings: Screenings[] = [];
   screenings: Screenings[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
     // Change this to search the screenings by movie id
-    this.http.get<[Screenings]>('http://127.0.0.1:8080/screenings').subscribe((data: Screenings[]) => {
+    /*this.http.get<[Screenings]>('http://127.0.0.1:8080/screenings').subscribe((data: Screenings[]) => {
       this.screenings = data;
-    })
+    })*/
   }
 
   enterNewScreening(screeningTicket:string, screeningDate:string, screeningTime:string, screeningMovie:string) {
@@ -27,8 +28,14 @@ export class AdminComponent implements OnInit {
     })
   }
 
-  selectScreening(screening: Screenings): void {
+  selectScreening(screening: string): void {
+    this.http.get<[Screenings]>('http://127.0.0.1:8080/screenings/?movieId='+screening).subscribe((data: Screenings[]) => {
+      this.screenings = data;
+    })
+  }
 
+  changeButton(screening: Screenings): void {
+    
   }
 
 }

8cb8220ce75ead72111bbf4638a1262235be5e12
Adrian <acliteur2001@gmail.com>
2022-10-31 23:19:38 -0400

    Created purchase-history Angular component

--- estore-ui/angular-movie-screening-estore/src/app/app.module.ts
+++ estore-ui/angular-movie-screening-estore/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CompletedPurchaseComponent } from './completed-purchase/completed-purch
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
 import { ScreeningsComponent } from './screenings/screenings.component';
+import { PurchaseHistoryComponent } from './purchase-history/purchase-history.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { ScreeningsComponent } from './screenings/screenings.component';
     CompletedPurchaseComponent,
     LoginComponent,
     AdminComponent,
-    ScreeningsComponent
+    ScreeningsComponent,
+    PurchaseHistoryComponent
   ],
   imports: [
     BrowserModule,

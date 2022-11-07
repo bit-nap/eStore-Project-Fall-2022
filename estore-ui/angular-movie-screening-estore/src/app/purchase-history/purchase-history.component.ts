e98b6657b1b2a94650652ef0f7181d58f4e0760e
Adrian <acliteur2001@gmail.com>
2022-11-07 03:47:07 -0500

    Purchase-History Account and Account orders, testing API server

--- estore-ui/angular-movie-screening-estore/src/app/purchase-history/purchase-history.component.ts
+++ estore-ui/angular-movie-screening-estore/src/app/purchase-history/purchase-history.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { LoggedInAccountService } from '../logged-in-account.service';
+// Class interfaces
 import { Order } from '../Order';
+import { Accounts } from '../Accounts';
+// Service
+import { LoggedInAccountService } from '../logged-in-account.service';
 
 import { HttpClient } from '@angular/common/http';
 
@@ -14,19 +17,35 @@ export class PurchaseHistoryComponent implements OnInit {
   // Array of Order objects to store objects from the JSON
   orders: Order[] = [];
 
+  // The account of the user logged in
+  user: Accounts = {
+    id: -1,
+    username: "",
+    password: ""
+  }
+
   /**
    * Required objects for deserializing the objects in the JSON based on user logged
    *
    * @param LoginService
    * @param http
    */
-  constructor(private LoginService: LoggedInAccountService, private http: HttpClient) { }
+  constructor(private login: LoggedInAccountService, private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<[Order]>('http://127.0.0.1:8080/orders').subscribe((orders: Order[]) => {
-      this.orders = orders;
+    this.getAccount;
+    this.getAccountOrders;
+  }
+
+  getAccount(): void {
+    this.http.get<Accounts>('http://127.0.0.1:8080/accounts/' + this.login.getUsername).subscribe((account_data: Accounts) => {
+      this.user = account_data;
     })
   }
 
-
+  getAccountOrders(): void {
+    this.http.get<[Order]>('http://127.0.0.1:8080/orders/?accountId=' + this.user.id).subscribe((orders_data: Order[]) => {
+      this.orders = orders_data;
+    })
+  }
 }

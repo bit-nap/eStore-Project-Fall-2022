d9cb81ac629c825b0893cb938ca7ded17e00b127
Adrian <acliteur2001@gmail.com>
2022-11-14 16:04:23 -0500

    Ambigiously empty orders array

--- estore-ui/angular-movie-screening-estore/src/app/purchase-history/purchase-history.component.ts
+++ estore-ui/angular-movie-screening-estore/src/app/purchase-history/purchase-history.component.ts
@@ -3,12 +3,9 @@ import { Component, OnInit } from '@angular/core';
 // Class interfaces
 import { Order } from '../order';
 import { Screening } from '../screening';
-import { Movie } from '../movie';
 
 // Service for logged in user
 import { LoggedInAccountService } from '../logged-in-account.service';
-import { ScreeningSelectorService } from '../screening-selector.service';
-import { MovieSelectorService } from '../movie-selector.service';
 
 // HTTP calls
 import { HttpClient } from '@angular/common/http';
@@ -23,11 +20,7 @@ export class PurchaseHistoryComponent implements OnInit {
   orders: Order[] = [];
 
   // Array of Screening objects for screening details
-  screening?: Screening;
   screenings: Screening[] = [];
-
-  // Array of Movie objects for movie details (movie poster URL)
-  movies: Movie[] = [];
 
   /**
    * Required objects for deserializing the objects in the JSON based on user logged
@@ -40,20 +33,11 @@ export class PurchaseHistoryComponent implements OnInit {
   ngOnInit(): void {
     this.checkAccount();
     this.getAccountOrders();
+    console.log(this.orders);
+    this.getScreeningsFromOrders();
     // this.setArrays();
     // this.printArrays();
   }
-
-  setScreening(screeningId: number): void {
-    this.http.get<Screening>('http://localhost:8080/screenings/' + screeningId).subscribe((screening_data: Screening) => {
-      this.screening = screening_data;
-    })
-  }
-
-  getScreeningId(): number {
-    return <number> this.screening?.id;
-  }
-
 
   checkAccount(): void {
     if (this.login.getId() === -1)
@@ -67,6 +51,7 @@ export class PurchaseHistoryComponent implements OnInit {
       if (orders_data != null && orders_data.length > 0) {
         // Reversing the list of orders to display from most recent to oldest (higher id is more recent)
         this.orders = orders_data.reverse();
+        console.log("filled orders: " + this.orders.length);
         document.getElementById("getOrdersMessage")!.innerHTML = "Displaying " + this.orders.length + " orders";
       } else {
         document.getElementById("getOrdersMessage")!.innerHTML = "No orders found";
@@ -74,33 +59,44 @@ export class PurchaseHistoryComponent implements OnInit {
     })
   }
 
-  setArrays(): void {
-    var i: number = 0;
-    for (var order of this.orders) {
-      this.http.get<Screening>('http://127.0.0.1:8080/screenings/' + order.screeningId).subscribe((screening_data: Screening) => {
+  getScreeningsFromOrders(): void {
+    console.log("TEST");
+    console.log("orders length: " + this.orders.length);
+    for (var i = 0; i < this.orders.length; i++) {
+      this.http.get<Screening>('http://localhost:8080/screenings/' + this.orders[i].screeningId).subscribe((screening_data: Screening) => {
         this.screenings[i] = screening_data;
-        console.log("length of screenings array: " + this.screenings.length);
-        this.http.get<Movie>('http://127.0.0.1:8080/movies/' + screening_data.movieId).subscribe((movie_data: Movie) => {
-          this.movies[i] = movie_data;
-          console.log("length of movies array: " + this.movies.length);
-        })
       })
-      i++;
     }
+    console.log("loop finished");
   }
 
-  printArrays(): void {
-    var i: number = 0;
-    for (var order of this.orders) {
-      console.log(i + ": " + this.orders[i].id);
-      console.log(i + ": " + this.screenings[i].id);
-      console.log(i + ": " + this.movies[i].id);
-      // document.getElementById("getOrdersMessage")!.innerHTML = i + ": " + this.orders[i].id;
-      // document.getElementById("getOrdersMessage")!.innerHTML = i + ": " + this.screenings[i].id;
-      // document.getElementById("getOrdersMessage")!.innerHTML = i + ": " + this.movies[i].id;
-      i++;
-    }
-  }
+  // setArrays(): void {
+  //   var i: number = 0;
+  //   for (var order of this.orders) {
+  //     this.http.get<Screening>('http://127.0.0.1:8080/screenings/' + order.screeningId).subscribe((screening_data: Screening) => {
+  //       this.screenings[i] = screening_data;
+  //       console.log("length of screenings array: " + this.screenings.length);
+  //       this.http.get<Movie>('http://127.0.0.1:8080/movies/' + screening_data.movieId).subscribe((movie_data: Movie) => {
+  //         this.movies[i] = movie_data;
+  //         console.log("length of movies array: " + this.movies.length);
+  //       })
+  //     })
+  //     i++;
+  //   }
+  // }
+
+  // printArrays(): void {
+  //   var i: number = 0;
+  //   for (var order of this.orders) {
+  //     console.log(i + ": " + this.orders[i].id);
+  //     console.log(i + ": " + this.screenings[i].id);
+  //     console.log(i + ": " + this.movies[i].id);
+  //     document.getElementById("getOrdersMessage")!.innerHTML = i + ": " + this.orders[i].id;
+  //     document.getElementById("getOrdersMessage")!.innerHTML = i + ": " + this.screenings[i].id;
+  //     document.getElementById("getOrdersMessage")!.innerHTML = i + ": " + this.movies[i].id;
+  //     i++;
+  //   }
+  // }
 
   // setScreeningMovie(screeningId: number): void {
   //   this.http.get<Screening>('http://localhost:8080/screenings/' + screeningId).subscribe((screening_data: Screening) => {
@@ -115,15 +111,20 @@ export class PurchaseHistoryComponent implements OnInit {
   //   })
   // }
 
-  // getScreenings(): void {
-  //   this.http.get<[Screening]>('http://localhost:8080/screenings').subscribe((screening_data: [Screening]) => {
-  //     this.screenings = screening_data;
-  //   })
-  // }
-
   // getMovies(): void {
   //   this.http.get<[Movie]>('http://localhost:8080/movies').subscribe((movie_data: [Movie]) => {
   //     this.movies = movie_data;
   //   })
   // }
+
+  // setScreening(screeningId: number): void {
+  //   this.http.get<Screening>('http://localhost:8080/screenings/' + screeningId).subscribe((screening_data: Screening) => {
+  //     this.screening = screening_data;
+  //   })
+  // }
+
+  // getScreeningId(): number {
+  //   return <number> this.screening?.id;
+  // }
+
 }

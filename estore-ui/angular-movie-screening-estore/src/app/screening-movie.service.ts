5105415840a16686d211a5e4739fda6176147f01
Adrian <acliteur2001@gmail.com>
2022-11-24 19:22:58 -0500

    Detailed purchase history working with ScreeningMovie Service

--- estore-ui/angular-movie-screening-estore/src/app/screening-movie.service.ts
+++ estore-ui/angular-movie-screening-estore/src/app/screening-movie.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Screening } from './screening';
 import { Movie } from './movie';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,25 +11,61 @@ import { HttpClient } from '@angular/common/http';
  * Service class for selecting and obtaining data from Screenings and Movies
  */
 export class ScreeningMovieService {
-  screenings: Screening[] = [];
+  // screenings: Screening[] = [];
+  screeningMap = new Map<number, Screening>();
+  movieMap = new Map<number, Movie>();
 
   constructor(private http: HttpClient) {
-    this.setScreenings();
+    this.getScreenings();
+    this.getMovies();
   }
 
-  setScreenings(): void {
+  getScreenings(): void {
     this.http.get<[Screening]>('http://localhost:8080/screenings/').subscribe((screenings_data: Screening[]) => {
-      this.screenings = screenings_data;
+      // this.screenings = screenings_data;
+      this.setScreeningMap(screenings_data);
     })
   }
 
-  getScreeningTime(screeningId: number): String {
-    for (var screening of this.screenings) {
-      if (screening.id == screeningId) {
-        return <String> screening.time;
-      }
+  setScreeningMap(screenings: Screening[]): void {
+    for (var screening of screenings) {
+      this.screeningMap.set(screening.id, screening);
     }
-    console.log("Screening Time not found");
-    return "Time not found";
+  }
+
+  getMovies(): void {
+    this.http.get<[Movie]>('http://localhost:8080/movies').subscribe((movies_data: Movie[]) => {
+      this.setMovieMap(movies_data);
+    })
+  }
+
+  setMovieMap(movies: Movie[]): void {
+    for (var movie of movies) {
+      this.movieMap.set(movie.id, movie);
+    }
+  }
+
+  getScreeningTime(screeningId: number): String {
+    return <String> this.screeningMap.get(screeningId)?.time;
+  }
+
+  getScreeningDate(screeningId: number): String {
+    return <String> this.screeningMap.get(screeningId)?.date;
+  }
+
+  getScreeningSeats(screeningId: number): Boolean[][] {
+    return <Boolean[][]> this.screeningMap.get(screeningId)?.seats;
+  }
+
+  getScreeningMovieId(screeningId: number): number {
+    return <number> this.screeningMap.get(screeningId)?.movieId;
+  }
+
+  getMovieTitle(screeningId: number): String {
+    return <String> this.movieMap.get(this.screeningMap.get(screeningId)!.movieId)?.title;
+  }
+
+  getMoviePoster(screeningId: number): String {
+    return <String> this.movieMap.get(this.screeningMap.get(screeningId)!.movieId)?.poster;
   }
 }
